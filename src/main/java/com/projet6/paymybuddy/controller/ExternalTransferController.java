d0f42b0f18881728b8eba8677f476d5990bfdb78
ZZAREV001 <zarev.zahari@gmail.com>
2023-04-29 22:46:25 +0200

    testing the ExternalTransfer controller

--- src/main/java/com/projet6/paymybuddy/controller/ExternalTransferController.java
+++ src/main/java/com/projet6/paymybuddy/controller/ExternalTransferController.java
@@ -56,4 +56,5 @@ public class ExternalTransferController {
         return "redirect:/user/extransfer";
     }
 
+
 }
--- src/test/java/com/projet6/paymybuddy/controller/ExternalTransferControllerTest.java
+++ src/test/java/com/projet6/paymybuddy/controller/ExternalTransferControllerTest.java
@@ -1,6 +1,7 @@
 package com.projet6.paymybuddy.controller;
 
 import com.projet6.paymybuddy.dto.ExternalTransferDto;
+import com.projet6.paymybuddy.model.BankAccount;
 import com.projet6.paymybuddy.service.BankAccountService;
 import com.projet6.paymybuddy.service.TransferService;
 import com.projet6.paymybuddy.service.UserService;
@@ -11,6 +12,8 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.security.web.csrf.DefaultCsrfToken;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -45,17 +48,64 @@ public class ExternalTransferControllerTest {
                 .andExpect(view().name("extransfer"));
     }
 
-    // Problem here: test fails
+    // Problem here: test fails if Spring Security is not performed
     @Test
     @WithMockUser(username = "testUser", authorities = "ROLE_USER")
-    public void testDoExternalTransfer() throws Exception {
+    public void itShouldDoExternalTransfer() throws Exception {
         ExternalTransferDto externalTransferDto = new ExternalTransferDto();
         externalTransferDto.setAmountUser(BigDecimal.valueOf(100));
 
+        // Create a CSRF token for the test request
+        CsrfToken csrfToken = new DefaultCsrfToken("X-CSRF-TOKEN", "_csrf", "test-csrf-token");
+
         mockMvc.perform(MockMvcRequestBuilders.post("/user/extransfer/doExternalTransfer")
                         .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                        .flashAttr("externalTransferDto", externalTransferDto))
+                        .flashAttr("externalTransferDto", externalTransferDto)
+                        .sessionAttr("org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository.CSRF_TOKEN", csrfToken)
+                        .param(csrfToken.getParameterName(), csrfToken.getToken()))
                 .andExpect(status().is3xxRedirection())
                 .andExpect(redirectedUrl("/user/extransfer"));
     }
+
+    @Test
+    @WithMockUser(username = "testUser", authorities = "ROLE_USER")
+    public void itShouldAddBankAccount() throws Exception {
+        BankAccount bankAccount = new BankAccount();
+        bankAccount.setIban("test-iban");
+        bankAccount.setBic("test-bic");
+        bankAccount.setBankName("test-bank");
+
+        // Create a CSRF token for the test request
+        CsrfToken csrfToken = new DefaultCsrfToken("X-CSRF-TOKEN", "_csrf", "test-csrf-token");
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/user/extransfer/addBankAccount")
+                        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                        .flashAttr("bankAccount", bankAccount)
+                        .sessionAttr("org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository.CSRF_TOKEN", csrfToken)
+                        .param(csrfToken.getParameterName(), csrfToken.getToken()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/user/extransfer"));
+    }
+
+    @Test
+    @WithMockUser(username = "testUser", authorities = "ROLE_USER")
+    public void itShouldDeleteBankAccount() throws Exception {
+        String iban = "test-iban";
+
+        // Create a CSRF token for the test request
+        CsrfToken csrfToken = new DefaultCsrfToken("X-CSRF-TOKEN", "_csrf", "test-csrf-token");
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/user/extransfert/deleteBankAccount")
+                        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                        .param("iban", iban)
+                        .sessionAttr("org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository.CSRF_TOKEN", csrfToken)
+                        .param(csrfToken.getParameterName(), csrfToken.getToken()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/user/extransfer"));
+    }
+
+
+
+
+
 }
